1f157464efadcc033434268863caad57ebed1bdf
jumping-albatross <98041351+jumping-albatross@users.noreply.github.com>
2023-12-09 11:55:17 +0000

    Updates

--- main.py
+++ main.py
@@ -8,3 +8,21 @@ else:
     with open('data09.txt') as f:
         raw = f.read().strip().split('\n')
 
+sequences = [list(map(int, x.strip().split())) for x in raw]
+
+# predict next number in sequence
+
+def differences(s):
+    new_s = []
+    for i in range(len(s) - 1):
+        new_s.append(s[i+1] - s[i])
+    return new_s
+
+s = 0
+
+for sequence in sequences:
+    rounds = [sequence]
+    while sum(rounds[-1]) != 0:
+        rounds.append(differences(rounds[-1]))
+    for i in range(len(rounds) - 1, 0, -1):
+        rounds[i - 1].append(rounds[i][-1] - rounds[i][-2])
