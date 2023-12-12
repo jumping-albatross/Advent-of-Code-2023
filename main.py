46623c01a198b0c17ca7856c7d2e7c8f7db0af18
jumping-albatross <98041351+jumping-albatross@users.noreply.github.com>
2023-12-12 20:51:36 +0000

    Working

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ raw = """???.### 1,1,3
 # with open("data12.txt") as r:
 #     raw = r.read().split("\n")
 
+rows = []
+
+for row in raw:
+    conditions, backup = row.split(' ')
+    conditions = list(conditions)
+    backup = [int(x) for x in backup.split(',')]
+    rows.append([conditions, backup])
+
