35f027bacc367f1588038443623cd2abbab3f7bc
jumping-albatross <98041351+jumping-albatross@users.noreply.github.com>
2023-12-16 20:15:39 -0500

    Day 16 Part 1 and 2

---
+++ day_16_1.py
@@ -0,0 +1,99 @@
+# Day 16 Part 1
+# Note: \ (backslash) has been replaced with ` (backtick) in raw data
+
+raw = """.|...`....
+|.-.`.....
+.....|-...
+........|.
+..........
+.........`
+..../.``..-
+.-.-/..|..
+.|....-|.`
+..//.|....""".split('\n')
+
+with open('data16.txt') as f:
+    raw = f.read().split('\n')
+
+
+def launch_beam(start_beam):
+    """Simulate the full route one beam of light takes. A beam is (dx, dy, x, y)."""
+
+    beams = {start_beam}
+
+    # set of visited tiles and beams
+    visited = set()
+
+    while len(beams) > 0:
+        new_beams = set()
+        for beam in beams:
+            # A beam is (dx, dy, x, y)
+            dx, dy, x, y = beam
+            x, y = x + dx, y + dy
+            
+            if 0 <= x < C and 0 <= y < R:
+                tile = TILES[y][x]
+            else:
+                continue  # skip this beam. It leaves the grid of tiles
+ 
+            if tile == "/":
+                # 1a. reflect beam (/\)
+                dx, dy = -dy, -dx
+                new_beams.add((dx, dy, x, y))
+            elif tile == "`":
+                # 1b. reflect beam (/\)
+                dx, dy = dy, dx
+                new_beams.add((dx, dy, x, y))
+            elif tile == "-":
+                if dy != 0:
+                    # 2a. reflect and split beam (-|)
+                    dx, dy = dy, dx
+                    new_beams.add((dx, dy, x, y))
+                    dx, dy = -dx, -dy
+                    new_beams.add((dx, dy, x, y))
+                else:
+                    # 2b. continue forward (-|)
+                    new_beams.add((dx, dy, x, y))
+            elif tile == "|":
+                if dx != 0:
+                    # 2c. reflect and split beam (-|)
+                    dx, dy = dy, dx
+                    new_beams.add((dx, dy, x, y))
+                    dx, dy = -dx, -dy
+                    new_beams.add((dx, dy, x, y))
+                else:
+                    # 2b. continue forward (-|)
+                    new_beams.add((dx, dy, x, y))
+            else:
+                # 2c. continue forward (.)
+                new_beams.add((dx, dy, x, y))
+
+        beams = set()
+
+        for beam in new_beams:
+            if beam not in visited:
+                beams.add(beam)
+                visited.add(beam)
+
+    tiles_visited = set()
+
+    for v in visited:
+        tiles_visited.add((v[2:]))
+
+    return len(tiles_visited)
+
+
+# * grid with energized tiles through which a beam has passed
+
+R = len(raw)
+C = len(raw[0])
+
+# * starting grid with tiles with spaces and mirrors
+
+TILES = raw
+
+starting_beam = (1, 0, -1, 0) # a beam is (dx, dy, x, y)
+
+tiles_energized = launch_beam(starting_beam)
+
+print(f"Tiles energized = {tiles_energized}")
