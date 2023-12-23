4355df6666cc666a456ff2d245d4c978e00d80eb
eric <eric.dunbar@tdsb.on.ca>
2023-12-22 21:02:37 -0500

    Day 20, Part 2

---
+++ day_20_2.py
@@ -0,0 +1,151 @@
+import heapq
+
+raw = """broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> in
+&in -> a""".split('\n')
+
+# raw = """broadcaster -> a
+# %a -> in, co
+# &in -> b
+# %b -> co
+# &co -> output""".split('\n')
+
+with open('day20.txt') as f:
+    raw = f.read().split('\n')
+
+modules = {}
+LOW = 0
+HIGH = 1
+
+# prepare modules
+for line in raw:
+    dest = line.split(' -> ')[1].split(', ')
+
+    # broadcast
+    if line[:len("broadcaster")] == "broadcaster":
+        modules['broadcaster'] = ['broadcaster', [*dest]]
+    else:
+        name = line[1:3].strip()
+        _type = 'flip-flop' if line[0] == "%" else 'conjunction'
+        v = [dest, LOW] if _type == 'flip-flop' else [dest, {}]
+        modules[name] = [_type, *v]
+
+tracked = {} # Part 2. Determine periods
+for r in raw:
+    src, dest = r.split(' -> ')
+    src = src[1:]
+    if 'ql' in dest:
+        tracked[src] = []
+
+
+# program the conjunctions
+for name, details in modules.items():
+    print(f"35{details = }")
+    for dest in details[1]:
+        if dest == 'output' or dest == 'rx':
+            continue
+        if modules[dest][0] == 'conjunction':
+            modules[dest][2][name] = LOW
+
+_c = 0
+lows = 0  # starts with 1 LOW button push
+highs = 0
+
+
+def push(signal, source, destinations):
+    """Push signal (LOW, HIGH), source module and destination module(s) onto queue"""
+    global _c, lows, highs
+    print(f"PUSH: {_c:3} {'HIGH' if signal else 'LOW':>5}, {source:>12} >>> {destinations}")
+    if signal == LOW:
+        lows += len(destinations)
+    else:
+        highs += len(destinations)
+    for d in destinations:
+        if type(d) == list:
+            print(f"#####     PUSH: {_c} {'HIGH' if signal else 'LOW'}, {source = }, {destinations = }")
+            print(f"{destinations = }... this is the problem")
+            raise Exception('oops. List')
+        _c += 1
+        # lows += signal == LOW
+        # highs += signal == HIGH
+        heapq.heappush(q, [_c, signal, source, d])
+
+
+is_trouble_shooting = False
+
+q = []
+for i in range(1000):
+    push(LOW, 'button', ['broadcaster'])
+    r = 0
+    while q:
+        r += 1
+        part_2_c, signal, src, name = heapq.heappop(q)
+        if name != 'output' and name != 'rx':
+            module = modules[name]
+        else:
+            continue
+
+        if is_trouble_shooting:
+            print(f"        Q heappop ({r}): {signal = }, {src = }, {name = }, {module = }")
+
+        if src == 'button':
+            if is_trouble_shooting:
+                print(f"TROUBLE 80: {module = }")
+                print(f"#*#*#*#* 81: {module = }")
+            push(LOW, 'broadcaster', module[1])
+        elif module[0] == 'flip-flop' and signal == LOW:
+            if is_trouble_shooting:
+                print(f"     flip-flop: {modules[name] = }")
+
+            modules[name][2] = 1 - modules[name][2]
+            push(modules[name][2], name, modules[name][1])
+        elif module[0] == 'conjunction':
+            # may the SOURCE be with you
+            # & only conjunctions lead to rx... maybe that's all that's needed
+            if is_trouble_shooting:
+                print(f"     conjunction: {modules[name] = }")
+            modules[name][2][src] = signal
+            if name in tracked and signal == HIGH:
+                tracked[name].append(part_2_c)
+            s = 0
+            for stat in modules[name][2].values():
+                s += stat
+                if is_trouble_shooting:
+                    print(f"***** {s = }, {len(modules[name][2].values()) = }")
+            if s == len(modules[name][2].values()):
+                push(LOW, name, modules[name][1])
+            else:
+                push(HIGH, name, modules[name][1])
+
+print(f"{lows = }, {highs = }, ({lows * highs = }) {lows * highs - 32000000 = }")
+
+sss = []
+cc = 0
+for i in tracked:
+    s = set()
+    v = tracked[i]
+    for jj in range(len(v) - 1):
+        s.add(v[jj + 1] - v[jj])
+    sss.append(s)
+
+for s in sss:
+    print(f"{len(s) = }")
+    print(s)
+    print()
+# flip-flop (%)
+# default: initially off
+# receive: if receive HIGH ignore
+# receive: if receive LOW flips states AND sends HIGH if turns ON or LOW if turns off
+
+# conjunction (&)
+# default: LOW
+# receive: upon receipt of pulse it remembers most recent state
+# receive, part 2: if all HIGH send LOW, otherwise send HIGH
+
+# broadcast module (broadcaster)
+# rebroadcasts received pulse to all destination modules
+
+# button module (button)
+# single LOW to broadcaster
