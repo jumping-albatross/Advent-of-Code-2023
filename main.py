9828bcdf6cd1b7132369b3f27d645a47d204075b
eric <eric.dunbar@tdsb.on.ca>
2023-12-23 19:42:56 -0500

    Day 21, Part 1

--- main.py
+++ main.py
@@ -1,78 +1,18 @@
 #https://github.com/oloturia/AoC2023/blob/main/day21/part1.py
 #
-testing = False
+testing = True
 
 if testing:
-    STEPS = 6
-    grid = """...........
-.....###.#.
-.###.##..#.
-..#.#...#..
-....#.#....
-.##..S####.
-.##..#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........""".split('\n')
+    grid = """
+
+1,0,1~1,2,1
+0,0,2~2,0,2
+0,2,3~2,2,3
+0,0,4~0,2,4
+2,0,5~2,2,5
+0,1,6~2,1,6
+1,1,8~1,1,9
+""".strip().split('\n')
 else:
-    STEPS = 64
-    with open('day_21.txt') as f:
+    with open('day_22.txt') as f:
         grid = f.read().strip().split('\n')
-
-for i, row in enumerate(grid):
-    if 'S' in row:
-        start = (i, row.find('S'))
-        grid[i] = row.replace('S', '.')
-        break
-
-q = set()
-q.add(start)
-
-for i in range(STEPS):
-    qq = set()
-    for (r, c) in q:
-        for (dr, dc) in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-            nr = r + dr
-            nc = c + dc
-            if grid[nr][nc] == '.':
-                qq.add((nr, nc))
-    q = qq
-
-grid_l = []
-for g in grid:
-    grid_l.append(list(g))
-
-for (r, c) in q:
-    grid_l[r][c] = "O"
-
-print(grid_l[start[0]][start[1]])
-
-check = """...........
-.....###.#.
-.###.##.O#.
-.O#O#O.O#..
-O.O.#.#.O..
-.##O.■####.
-.##.O#O..#.
-.O.O.O.##..
-.##.#.####.
-.##O.##.##.
-...........""".split('\n')
-matches = True
-
-grid_l[start[0]][start[1]] = "■"
-for r, line in enumerate(grid_l):
-    if abs(r - start[0]) < 6:
-        print(''.join(line))
-        if testing:
-            matches = matches and ''.join(line) == check[r]
-
-print(f"{len(q) = }")
-print(f"{start = }")
-if testing:
-    print(f"{matches = }")
-
-# 2925 too low
-# 3186 too low
-# 3666 is it https://github.com/stefanoandroni/advent-of-code/blob/master/2023/day-21/part-1/main.py
