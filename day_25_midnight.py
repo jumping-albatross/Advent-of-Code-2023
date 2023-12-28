a777639d884c7bc26e87c8c74d5428737455bf97
eric <eric.dunbar@tdsb.on.ca>
2023-12-28 08:53:18 -0500

    Day 25, Part 1

--- day_25_midnight.py
+++ day_25_midnight.py
@@ -2,15 +2,19 @@
 # https://www.reddit.com/r/adventofcode/comments/18qbsxs/comment/kf12xgf/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 # https://python-course.eu/applications-python/graphs-python.php
 # https://www.reddit.com/r/adventofcode/comments/18qbsxs/comment/keuafrl/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-# https://www.reddit.com/r/adventofcode/comments/18qbsxs/comment/kf94vru/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+
+The cut portion works well.
+
+Finding the edges to cut is less reliable and fails sometimes (random chance)
 
 import random
-from time import time, asctime
+from time import time
 
-print(f"Starting at {asctime()}...\n")
 testing = False
+has_cut = True
 
 if testing:
+    key_edges = (('hfx', 'pzl'), ('bvb', 'cmg'), ('jqt', 'nvd'))
     raw_connections = """jqt: rhn xhk nvd
 rsh: frs pzl lsr
 xhk: hfx
@@ -25,37 +29,52 @@ lsr: lhk
 rzs: qnr cmg lsr rsh
 frs: qnr lhk lsr"""
 else:
+    key_edges = (('fch', 'fvh'), ('jbz', 'sqh'), ('nvg', 'vfj'))
     with open('day25.txt') as f:
         raw_connections = f.read()
 
 connections = raw_connections.strip().splitlines()
 nodes = {}
-
-
-def add_node(nodes, n1, n2):
-    if n1 not in nodes:
-        nodes[n1] = {n2}
-    else:
-        nodes[n1].add(n2)
-
-
-# create the graph
 for line in connections:
     node, neighbours = line.split(': ')
-    neighbours = neighbours.split()
-    add_node(nodes, node, neighbours[0])
-
+    neighbours = neighbours.split(' ')
+    if node not in nodes:
+        nodes[node] = {neighbours[0]}
+    else:
+        nodes[node].add(neighbours[0])
     for _n in neighbours[1:]:
         nodes[node].add(_n)
 
     for _n in neighbours:
-        add_node(nodes, _n, node)
+        if _n not in nodes:
+            nodes[_n] = {node}
+        else:
+            nodes[_n].add(node)
 
+
+def cut_edges(edges):
+    # remove the given edges from the graph
+    for (c1, c2) in edges:
+        nodes[c1].remove(c2)
+        nodes[c2].remove(c1)
+        print(("Removed", f"{(c1, c2) = }"))
+
+
+# cut the nodes
+if has_cut:
+    cut_edges(key_edges)
+
+m_len = 0
+for n in nodes:
+    # print(n, nodes[n])
+    m_len = max(m_len, len(nodes[n]))
+
+from heapq import heappop, heappush
 
 def find_random_path(start_vertex, end_vertex, path=None):
     """ find a path from start_vertex to end_vertex
         in graph """
-    if path == None:  # TO DO conditional seems redundant
+    if path == None: # TO DO conditional seems redundant
         path = []
     graph = nodes
     path = path + [start_vertex]
@@ -73,16 +92,16 @@ def find_random_path(start_vertex, end_vertex, path=None):
                 return extended_path
     return None
 
-
-def find_random_path_iterate(start_vertex, end_vertex):
+def find_ni_random_path(start_vertex, end_vertex):
     """ iteratively find a path from start_vertex to end_vertex
         in graph """
+    graph = nodes
     stack = [(start_vertex, [start_vertex])]
     while stack:
         (vertex, path) = stack.pop()
         if vertex == end_vertex:
             return path
-        neighbours = list(nodes[vertex])
+        neighbours = list(graph[vertex])
         random.shuffle(neighbours)
         for neighbour in neighbours:
             if neighbour not in path:
@@ -91,10 +110,11 @@ def find_random_path_iterate(start_vertex, end_vertex):
     return None
 
 
+
 def find_path(start_vertex, end_vertex, path=None):
     """ find a path from start_vertex to end_vertex
         in graph """
-    if path == None:  # TO DO conditional seems redundant
+    if path == None: # TO DO conditional seems redundant
         path = []
     graph = nodes
     path = path + [start_vertex]
@@ -109,13 +129,11 @@ def find_path(start_vertex, end_vertex, path=None):
                 return extended_path
     return None
 
-
 node_frequencies = {}
 
 rounds = 0
-k1, k2 = list(nodes.keys()), list(nodes.keys())
+k1,k2 = list(nodes.keys()), list(nodes.keys())
 sz = len(k1)
-
 
 def return_edges(path):
     e = set()
@@ -123,23 +141,15 @@ def return_edges(path):
         e.add(tuple(sorted([path[i], path[i + 1]])))
     return e
 
-
-def cut_edges(edges=(('fch', 'fvh'), ('jbz', 'sqh'), ('nvg', 'vfj'))):
-    # cut the nodes
-    for (c1, c2) in edges:
-        nodes[c1].remove(c2)
-        nodes[c2].remove(c1)
-        print(("Removed", f"{(c1, c2) = }"))
-
-has_cut = True
 if not has_cut:
     # find nodes to cut
-    for i in range(25):
+    for i in range(50):
         random.shuffle(k1)
         random.shuffle(k2)
         for node_0 in zip(k1[:sz], k2[:sz]):
+            # print(node_0)
             rounds += 1
-            pt = find_random_path_iterate(node_0[0], node_0[1])
+            pt = find_ni_random_path(node_0[0], node_0[1])
             if pt != None:
                 for n in return_edges(pt):
                     if n in node_frequencies:
@@ -153,24 +163,24 @@ if not has_cut:
     for (x, _) in sorted(list(node_frequencies.items()), key=lambda x: x[1])[-3:]:
         s1.append(x)
 
-    print('Testing: ', sorted([('hfx', 'pzl'), ('bvb', 'cmg'), ('jqt', 'nvd')]))
-    print('         ', sorted(s1))
+    print('SHould be: ', sorted(key_edges))
+    print('         : ', sorted(s1), set(key_edges) == set(sorted(s1)))
 else:
-    cut_edges()
     set1 = set()
-    founder1 = 'fch'
+    founder1 = key_edges[0][0]
     set2 = set()
-    founder2 = 'fvh'
+    founder2 = key_edges[0][1]
     t1 = time()
     while len(set1) + len(set2) - len(nodes.keys()):
         for founder_n, set_n, set_other in zip([founder1, founder2], [set1, set2], [set2, set1]):
             for i in list(nodes.keys()):
-                if i in set_other or i in set_n:
-                    break
-                pt = find_random_path_iterate(i, founder_n)
-                if pt != None:
-                    set_n.add(i)
-                    break
+                for _ in range(1):
+                    if i in set_other or i in set_n:
+                        break
+                    pt = find_ni_random_path(i, founder_n)
+                    if pt != None:
+                        set_n.add(i)
+                        break
         print(f"{len(nodes.keys()) = }")
         print(f"{len(set1) = }")
         print(f"{len(set2) = }")
