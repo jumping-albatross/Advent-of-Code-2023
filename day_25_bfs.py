5a6e7832131b03acf4b1c899cc5a4d44ee645633
jumping-albatross <eric.dunbar@tdsb.on.ca>
2023-12-30 21:10:12 -0500

    bfs 25

--- day_25_bfs.py
+++ day_25_bfs.py
@@ -2,14 +2,16 @@
 # https://www.reddit.com/r/adventofcode/comments/18qbsxs/comment/kf12xgf/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 # https://python-course.eu/applications-python/graphs-python.php
 # https://www.reddit.com/r/adventofcode/comments/18qbsxs/comment/keuafrl/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+# https://en.wikipedia.org/wiki/Bipartite_graph
 
-# The cut portion works well.
-
-# Finding the edges to cut is less reliable and fails sometimes (random chance)
+# Finding which edges to cut to make this graph a bipartite graph fails sometimes while following the
+# random path finding algorithm (find_ni_random_path). Does changing it to a non-random path finding
+# algorithm change its behaviour? 30/12/23
 
 import random
 from time import time
 import heapq
+from typing import List, Tuple, Any
 
 testing = False
 to_search = True
@@ -35,32 +37,17 @@ else:
     with open('day25.txt') as f:
         raw_connections = f.read()
 
-connections = raw_connections.strip().splitlines()
-nodes = {}
-
-for line in connections:
-    node, neighbours = line.split(': ')
-    neighbours = neighbours.split(' ')
-    if node not in nodes:
-        nodes[node] = {neighbours[0]}
-    else:
-        nodes[node].add(neighbours[0])
-    for _n in neighbours[1:]:
-        nodes[node].add(_n)
-
-    for _n in neighbours:
-        if _n not in nodes:
-            nodes[_n] = {node}
-        else:
-            nodes[_n].add(node)
-
 
 def cut_edges(edges):
     # remove the given edges from the graph
+    print("Removing edges... ", end="")
+    ch = ""
     for (c1, c2) in edges:
         nodes[c1].remove(c2)
         nodes[c2].remove(c1)
-        print(f"Removed edge {(c1, c2)}")
+        print(f"{ch}{(c1, c2)}", end="")
+        ch = ", "
+    print()
 
 
 def find_ni_random_path(start_vertex, end_vertex):
@@ -81,46 +68,23 @@ def find_ni_random_path(start_vertex, end_vertex):
     return None
 
 
-def find_path(start_vertex, end_vertex, path=None):
-    """ find a path from start_vertex to end_vertex
-        in graph """
-    if path is None:  # TO DO conditional seems redundant
-        path = []
-    graph = nodes
-    path = path + [start_vertex]
-    if start_vertex == end_vertex:
-        return path
-    if start_vertex not in graph:
-        return None
-    for vertex in graph[start_vertex]:
-        if vertex not in path:
-            extended_path = find_path(vertex, end_vertex, path)
-            if extended_path:
-                return extended_path
-    return None
-
-
 def bfs(f1, f2, s1, s2, graph):
     for (f, s, other) in zip([f1, f2], [s1, s2], [s2, s1]):
         idx = 0
 
-        q = [(idx, f)]
+        q: list[tuple[int, str]] = [(idx, f)]
         while q:
             idx += 1
             _, current = heapq.heappop(q)
             neighbours = graph[current]
             for n in neighbours:
+                if n in other:
+                    print("This is not a bipartite graph")
+                    return 1
                 if n not in s:
                     s.add(n)
                     heapq.heappush(q, (idx, n))
-                if n in other:
-                    raise Exception("This is not a bipartite graph")
-
-
-edge_frequencies = {}
-
-k1, k2 = list(nodes.keys()), list(nodes.keys())
-sz = len(k1)
+    return 0
 
 
 def return_edges(path):
@@ -130,39 +94,59 @@ def return_edges(path):
     return e
 
 
-if to_search:
-    # find edges to cut
-    t1 = time()
-    _n = 5
+# try this 10 times
+for _ in range(40):
 
-    for i in range(_n):
-        print(f"Looking for edges to cut ({i + 1} of {_n})")
-        random.shuffle(k1)
-        random.shuffle(k2)
-        for node_0 in zip(k1[:sz], k2[:sz]):
-            pt = find_ni_random_path(node_0[0], node_0[1])
-            if pt is not None:
-                for n in return_edges(pt):
-                    if n in edge_frequencies:
-                        edge_frequencies[n] += 1
-                    else:
-                        edge_frequencies[n] = 1
+    connections = raw_connections.strip().splitlines()
+    nodes = {}
 
-    print('\n', time() - t1, '\n')
+    for line in connections:
+        node, neighbours = line.split(': ')
+        neighbours = neighbours.split(' ')
+        if node not in nodes:
+            nodes[node] = {neighbours[0]}
+        else:
+            nodes[node].add(neighbours[0])
+        for _n in neighbours[1:]:
+            nodes[node].add(_n)
 
-    print(sorted(list(edge_frequencies.items()), key=lambda z: z[1])[-10:])
+        for _n in neighbours:
+            if _n not in nodes:
+                nodes[_n] = {node}
+            else:
+                nodes[_n].add(node)
 
-    s1 = []
-    for (x, _) in sorted(list(edge_frequencies.items()), key=lambda z: z[1])[-3:]:
-        s1.append(x)
+    k1, k2 = list(nodes.keys()), list(nodes.keys())
 
-    print('Expected: ', sorted(key_edges))
-    print('Observed: ', sorted(s1), set(key_edges) == set(s1))
-    print()
+    edge_frequencies = {}
 
-    key_edges = s1
+    if to_search:
+        # find edges to cut
+        # @ 20 no failures in 40 tests
+        t1 = time()
+        _n = 10
 
-if to_cut:
+        for i in range(_n):
+            random.shuffle(k1)
+            random.shuffle(k2)
+            for node_0 in zip(k1, k2):
+                pt = find_ni_random_path(node_0[0], node_0[1])
+                if pt is not None:
+                    for n in return_edges(pt):
+                        if n in edge_frequencies:
+                            edge_frequencies[n] += 1
+                        else:
+                            edge_frequencies[n] = 1
+        print(f"Finished looking for edges to cut ({_n} rounds) in {time() - t1:.1f} s.")
+
+        _s = []
+        for (x, _) in sorted(list(edge_frequencies.items()), key=lambda z: z[1])[-3:]:
+            _s.append(x)
+
+        print('Observed: ', sorted(_s), set(key_edges) == set(_s))
+
+        key_edges = _s
+
     # cut the nodes
     cut_edges(key_edges)
 
@@ -170,29 +154,10 @@ if to_cut:
     founder1, founder2 = key_edges[0]
 
     t1 = time()
-    r = 1
+    bfs(founder1, founder2, set1, set2, nodes)
+    print(f"Time needed to confirm whether this is a bipartite graph: {time() - t1:.5f} s")
 
-    if t1 > 1:
-        bfs(founder1, founder2, set1, set2, nodes)
-    else:
-
-        while len(set1) + len(set2) - len(nodes.keys()):
-            for founder_n, set_a, set_b in zip([founder1, founder2], [set1, set2], [set2, set1]):
-                for i in list(nodes.keys()):
-                    for _ in range(1):
-                        if i in set_b or i in set_a:
-                            break
-                        pt = find_ni_random_path(i, founder_n)
-                        if pt is not None:
-                            set_a.add(i)
-                            break
-
-    print()
-    print(f"Round {r}: {len(nodes.keys()) = }")
-    print(f"{len(set1) = },  {len(set2) = }")
+    print(f"{len(nodes.keys()) = }, {len(set1) = },  {len(set2) = }")
     print(f"{len(set1) + len(set2) - len(nodes.keys()) = }")
     print(f"{len(set1) * len(set2) = }")
-    r += 1
-    if r > 16:
-        raise Exception(f"WARNING: {len(set1) * len(set2) = } may not a valid result")
-    print('\n', time() - t1, '\n')
+    print()
